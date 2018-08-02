ce1832bef07426ff9a9cb6a16f13c5c7aca3dbf9
Tim Schindler <xh3b4sd@gmail.com>
2018-08-02 23:06:54 +0200

    implements management of deleting cluster status

--- create.go
+++ create.go
@@ -285,7 +285,6 @@ func (r *Resource) computeCreateEventPatches(ctx context.Context, accessor metav
 
 	// TODO emit metrics when update did not complete within a certain timeframe
 	// TODO update status condition when guest cluster is migrating from creating to created status
-	// TODO update status condition to Deleting during delete event when guest cluster is being deleted
 
 	return patches, nil
 }
