0aa47b290d2dd251adc9ea50962ef20a5b7c42a1
Thomas Fussell <thomas@giantswarm.io>
2021-02-22 17:37:20 -0400

    Handle more workload cluster API errors (#72)
    * update k8sclient for timeout error
    
    * handle error, update errors lib
    
    * add changelog
    
    * use released versions
    
    * add debug log when api is not available

--- create.go
+++ create.go
@@ -251,7 +251,10 @@ func (r *Resource) computeCreateEventPatches(ctx context.Context, obj interface{
 				RestConfig: restConfig,
 			}
 			k8sClients, err := k8sclient.NewClients(clientsConfig)
-			if err != nil {
+			if tenant.IsAPINotAvailable(err) || k8sclient.IsTimeout(err) {
+				r.logger.Debugf(ctx, "did not create Kubernetes client for tenant cluster, api is not yet available, canceling resource")
+				return nil, nil
+			} else if err != nil {
 				return nil, microerror.Mask(err)
 			}
 
